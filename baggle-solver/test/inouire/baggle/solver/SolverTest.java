bfe1a4989c8f2cda0eb283e9824bcafbc12bfe5d
inouire <edouard@melix.net>
2014-12-10 22:52:33 +0100

    (Final) update of french dic thanks to Bernard J.

--- baggle-solver/test/inouire/baggle/solver/SolverTest.java
+++ baggle-solver/test/inouire/baggle/solver/SolverTest.java
@@ -1,20 +1,3 @@
- /* Copyright 2009-2013 Edouard Garnier de Labareyre
-  *
-  * This file is part of B@ggle.
-  *
-  * B@ggle is free software: you can redistribute it and/or modify
-  * it under the terms of the GNU General Public License as published by
-  * the Free Software Foundation, either version 3 of the License, or
-  * (at your option) any later version.
-  *
-  * B@ggle is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  * GNU General Public License for more details.
-  *
-  * You should have received a copy of the GNU General Public License
-  * along with B@ggle.  If not, see <http://www.gnu.org/licenses/>.
-  */
 package inouire.baggle.solver;
 
 import java.util.ArrayList;
@@ -60,7 +43,7 @@ public class SolverTest {
     public void testGridNbPoints()
     {
         System.out.println("Test grid number of points");
-        assertEquals(351,Solver.getNbPoints(test_results_4x4));
+        assertEquals(355,Solver.getNbPoints(test_results_4x4));
     }
     
      /**
